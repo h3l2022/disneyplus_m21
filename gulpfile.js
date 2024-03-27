e4651e2094bb2e3907bf97725fd7f38b856d373d
Lucas Mario <lucasmariocontato@gmail.com>
2024-03-27 13:48:09 -0300

    setup do projeto

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function styles(){
-    return gulp.src('./src/styles/* .scss')
+    return gulp.src('./src/styles/*.scss')
     .pipe(sass({ outputStyle: 'compressed'}))
-    .pipe(gulp.dest(' ./dist/css'));
+    .pipe(gulp.dest('./dist/css'));
 }
 
 exports.default = styles;
